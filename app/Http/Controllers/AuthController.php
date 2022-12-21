1249bdaded5f63a2de7e4eedc69bf4ed09619a4d
carloseduardoalvesviana <eduardostrink@gmail.com>
2022-12-21 00:01:30 -0300

    chore: add login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -20,5 +21,22 @@ class AuthController extends Controller
         ]);
 
         return response($user, Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request)
+    {
+        $isAuth = Auth::attempt($request->only('email', 'password'));
+
+        if(!$isAuth) {
+            return response([
+                'error' => 'Invalid credentials'
+            ], Response::HTTP_UNAUTHORIZED);
+        }    
+
+        /** @var User $user */
+        $user = Auth::user();
+        $token = $user->createToken('token')->plainTextToken;
+
+        return response(['token' => $token]);
     }
 }
