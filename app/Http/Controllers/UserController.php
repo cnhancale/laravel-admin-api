04ee11c528e4f9199b6b3540ff1d5f9f506473e1
carloseduardoalvesviana <eduardostrink@gmail.com>
2023-01-03 23:01:57 -0300

    add: one to many relations

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,7 +26,7 @@ class UserController extends Controller
 
     public function show($id)
     {
-        $user = User::find($id);
+        $user = User::with('role')->find($id);
         return response($user, Response::HTTP_OK);
     }
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -12,31 +12,22 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'first_name',
         'last_name',
         'email',
         'password',
+        'role_id'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
-        'password'
+        'password', 'created_at', 'updated_at'
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
